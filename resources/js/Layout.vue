39bf9c0b1a7b72865dd611c2e70f3521c404741c
Daniël van der Spoel <d.vanderspoel@mediamere.com>
2021-09-20 17:22:33 +0200

    add basic laravel template

---
+++ resources/js/Layout.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <slot />
+    </div>
+</template>
+
+<script>
+import Navbar from "./components/Navbar";
+export default {
+name: "Layout",
+    components: {Navbar}
+}
+</script>
+
+<style scoped>
+
+</style>
