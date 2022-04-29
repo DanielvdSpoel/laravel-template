130f6af7397c48b3d9c98601d14d57ec44baba3f
Daniël van der Spoel <contact@danielvdspoel.nl>
2022-04-29 13:22:31 +0200

    Update Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,24 @@
 require('./bootstrap');
-import Vue from 'vue'
-import { createInertiaApp } from '@inertiajs/inertia-vue'
+import { createApp, h } from 'vue'
+import { createInertiaApp } from '@inertiajs/inertia-vue3'
+import { createI18n } from 'vue-i18n'
 
 createInertiaApp({
     resolve: name => require(`./Pages/${name}`),
-    setup({ el, App, props }) {
-        new Vue({
-            render: h => h(App, props),
-        }).$mount(el)
+    setup({ el, App, props, plugin }) {
+        const messages = JSON.parse(props.initialPage.props.translations);
+
+        const i18n = createI18n({
+            locale: 'nl',
+            fallbackLocale: 'en',
+            globalInjection: true,
+            messages,
+        })
+
+        createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(i18n)
+            .mixin({ methods: { route } })
+            .mount(el)
     },
 })
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,11 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+    @routes
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <script src="https://kit.fontawesome.com/ddae002269.js" crossorigin="anonymous"></script>
+    @inertiaHead
 </head>
 <body>
 @inertia
