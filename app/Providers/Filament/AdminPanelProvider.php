0f64f9dabf2cf309400b9f3ba66e6644275a8d38
Daniël van der Spoel <contact@danielvdspoel.nl>
2023-08-27 20:33:26 +0200

    Add environment indicator

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -18,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use pxlrbt\FilamentEnvironmentIndicator\EnvironmentIndicatorPlugin;
 use pxlrbt\FilamentEnvironmentIndicator\FilamentEnvironmentIndicatorPlugin;
 
 class AdminPanelProvider extends PanelProvider
@@ -37,6 +38,10 @@ class AdminPanelProvider extends PanelProvider
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
                 Pages\Dashboard::class,
+            ])
+            ->plugins([
+                EnvironmentIndicatorPlugin::make()
+                ->visible(fn() => app()->environment() !== 'production')
             ])
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
